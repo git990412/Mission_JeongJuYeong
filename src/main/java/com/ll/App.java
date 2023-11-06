cc69ca84715a3ef5e60de866a36e1a29b79c77f3
git990412 <wjdwn282@gmail.com>
2023-11-06 10:26:52 +0900

    목록, 종료료 기능 추가

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,4 +1,9 @@
 package com.ll;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -7,9 +12,13 @@ public class App {
     static void run(){
         Scanner sc = new Scanner(System.in);
         List<LifeQuotes> lqList = new ArrayList<>();
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
         String command = "";
         String quotes = "";
         String author = "";
+
+
 
         System.out.println("== 명언 앱 ==");
         while (true){
@@ -30,6 +39,24 @@ public class App {
                     System.out.println(Integer.toString(index) + "번 명언이 등록되었습니다.");
 
                     break;
+                case "목록":
+                    System.out.println("번호 / 작가 / 명언");
+                    System.out.println("----------------------");
+
+                    for (LifeQuotes temp : lqList) {
+                        String idx = Integer.toString(temp.getId());
+                        System.out.println(idx + " / " + temp.getAuthor() + " / " + temp.getQuotes());
+                    }
+
+                    break;
+                case "종료":
+                    try{
+                        objectMapper.writeValue(new File("List.json"), lqList);
+                    }catch (IOException e){
+                        e.printStackTrace();
+                    }
+                default:
+
             }
         }
     }
